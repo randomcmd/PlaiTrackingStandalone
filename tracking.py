f366f94ddb59f4391ebfb1c35f44b977df9bfee3
randomcmd <63674603+randomcmd@users.noreply.github.com>
2025-10-01 19:16:52 +0200

    fix

--- tracking.py
+++ tracking.py
@@ -1,14 +1,7 @@
 import torch
 import cv2
-import argparse
-import utils.saveload
-import utils.basic
-import utils.improc
-import PIL.Image
 import numpy as np
-import os
 from prettytable import PrettyTable
-import time
 from nets.alltracker import Net
 
 from alltracker.demo import count_parameters
