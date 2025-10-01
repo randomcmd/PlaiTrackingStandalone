32d2836a7b0f51ad4706fb70069038c09d42e3a1
randomcmd <63674603+randomcmd@users.noreply.github.com>
2025-10-01 19:16:15 +0200

    starting tracking

--- tracking.py
+++ tracking.py
@@ -1,2 +1,26 @@
-def run_tracking_model():
-    pass
+import torch
+import cv2
+import argparse
+import utils.saveload
+import utils.basic
+import utils.improc
+import PIL.Image
+import numpy as np
+import os
+from prettytable import PrettyTable
+import time
+from nets.alltracker import Net
+
+from alltracker.demo import count_parameters
+
+
+def run_tracking_model(video_path: str, tiny = True) -> None:
+    if tiny:
+        model = Net(args.window_len, use_basicencoder=True, no_split=True)
+        url = "https://huggingface.co/aharley/alltracker/resolve/main/alltracker_tiny.pth"
+    else:
+        model = Net(args.window_len)
+        url = "https://huggingface.co/aharley/alltracker/resolve/main/alltracker.pth"
+
+    count_parameters(model)
+    run(model, None)
