8b7eaa3ff069c97032a66b9d5fc3f07dcec6e692
randomcmd <63674603+randomcmd@users.noreply.github.com>
2025-10-01 18:35:25 +0200

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 set -e
-cd alltracker || echo "\"alltracker\" directory not found. Did you forget to clone recursively?" && exit
+cd alltracker || (echo "\"alltracker\" directory not found. Did you forget to clone recursively?"; exit)
 pip install -r requirements.txt
-./download_reference_model.sh
+bash ./download_reference_model.sh
 cd ..
 
-cd Depth-Anything-V2  || echo "\"Depth-Anything-V2\" directory not found. Did you forget to clone recursively?" && exit
+cd Video-Depth-Anything  || (echo "\"Depth-Anything-V2\" directory not found. Did you forget to clone recursively?"; exit)
 pip install -r requirements.txt
+bash ./get_weights.sh
 cd checkpoints
 wget https://huggingface.co/depth-anything/Depth-Anything-V2-Small/resolve/main/depth_anything_v2_vits.pth?download=true
 cd ..
