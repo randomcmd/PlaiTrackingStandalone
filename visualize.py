5505809f4441b2d781557e2e918e02adbd299f88
randomcmd <63674603+randomcmd@users.noreply.github.com>
2025-10-02 18:40:19 +0200

    fix

--- visualize.py
+++ visualize.py
@@ -6,7 +6,7 @@ def visualize(video_path: str, data: torch.Tensor, output_path: str):
     source = cv2.VideoCapture(video_path)
     fps = source.get(cv2.CAP_PROP_FPS)
     resolution = [int(source.get(cv2.CAP_PROP_FRAME_WIDTH)), int(source.get(cv2.CAP_PROP_FRAME_HEIGHT))]
-    output = cv2.VideoWriter(output_path, cv2.VideoWriter.fourcc(*'MP4V'), fps, resolution)
+    output = cv2.VideoWriter(output_path, cv2.VideoWriter.fourcc(*'mp4v'), fps, resolution)
 
     while True:
         ret, frame = source.read()
