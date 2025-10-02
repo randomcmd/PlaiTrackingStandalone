d8ac7fb9c469e42e04ad59598554ddb706846712
randomcmd <63674603+randomcmd@users.noreply.github.com>
2025-10-02 10:31:23 +0200

    fix

--- tracking.py
+++ tracking.py
@@ -10,7 +10,8 @@ from alltracker.nets.alltracker import Net
 
 from alltracker_demo_modified import count_parameters, run
 
-def run_tracking_model(video_path: str, tiny = True) -> torch.Tensor:
+
+def run_tracking_model(video_path: str, tiny=True) -> torch.Tensor:
     torch.set_grad_enabled(False)
     window_len = 16
     if tiny:
@@ -29,21 +30,23 @@ def run_tracking_model(video_path: str, tiny = True) -> torch.Tensor:
 
     return xy
 
+
 class Args:
     def __init__(self, video_path, window_len):
-            self.ckpt_init = ''
-            self.mp4_path = video_path
-            self.query_frame = 0
-            self.image_size = 1024
-            self.max_frames = 400
-            self.inference_iters = 4
-            self.window_len = window_len
-            self.rate = 2
-            self.conf_thr = 0.1
-            self.bkg_opacity = 0.5
-            self.vstack = False
-            self.hstack = False
-            self.tiny = True
+        self.ckpt_init = ''
+        self.mp4_path = video_path
+        self.query_frame = 0
+        self.image_size = 1024
+        self.max_frames = 400
+        self.inference_iters = 4
+        self.window_len = window_len
+        self.rate = 2
+        self.conf_thr = 0.1
+        self.bkg_opacity = 0.5
+        self.vstack = False
+        self.hstack = False
+        self.tiny = True
+
 
 def extract_closest_trajectory(tracking: torch.Tensor, x: int, y: int) -> torch.Tensor:
     if tracking.ndim != 3 or tracking.shape[2] != 2:
@@ -55,4 +58,4 @@ def extract_closest_trajectory(tracking: torch.Tensor, x: int, y: int) -> torch.
     distance_squared = ((xy0 - target) ** 2).sum(dim=1)
     best_tracker_index = torch.argmin(distance_squared).item()
 
-    return tracking[best_tracker_index]
+    return tracking[:, best_tracker_index, :]
