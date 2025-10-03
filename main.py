812d15687456cca668f2b9a396780c4076f65eed
randomcmd <63674603+randomcmd@users.noreply.github.com>
2025-10-03 16:03:50 +0200

    moved args to __main__

--- main.py
+++ main.py
@@ -9,18 +9,7 @@ from tracking import run_tracking_model, extract_closest_trajectory
 from visualize import visualize
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog='tracking',
-        description='tracks a singular point on a video in 3d space',
-        epilog='this is meant for a comfyui node to spawn'
-    )
-    parser.add_argument('video', help='the video file to track')
-    parser.add_argument('x', type=int, help='the x coordinate of the tracking point')
-    parser.add_argument('y', type=int, help='the y coordinate of the tracking point')
-    parser.add_argument('--output', help='outputs debug video')
-    args = parser.parse_args()
-
+def main(args):
     print(f'Ran program with args: {args.video=} {args.x=} {args.y=}', file=sys.stderr)
 
     # TODO: Depth model and tracking model could be running in parallel
@@ -58,4 +47,14 @@ def apply_depth_data_to_tracking_data(tracking: torch.Tensor, depths: torch.Tens
     return depth_at_points
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='tracking',
+        description='tracks a singular point on a video in 3d space',
+        epilog='this is meant for a comfyui node to spawn'
+    )
+    parser.add_argument('video', help='the video file to track')
+    parser.add_argument('x', type=int, help='the x coordinate of the tracking point')
+    parser.add_argument('y', type=int, help='the y coordinate of the tracking point')
+    parser.add_argument('--output', help='outputs debug video')
+    args = parser.parse_args()
     main()
